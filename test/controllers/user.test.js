478b0d8f472d335032fcf3bc407835b60b675d4b
Matias Dumrauf <matias.dumrauf@gmail.com>
2019-02-10 19:20:27 -0300

    implemented test case for POST /api/users

---
+++ test/controllers/user.test.js
@@ -0,0 +1,32 @@
+
+const app = require('../');
+const request = require('supertest');
+
+const MongooseHelper = require('../helpers/mongoose');
+
+
+describe('Users controller', () => {
+    let mongoose;
+
+    before(async() => {
+        mongoose = await MongooseHelper.configure();
+    });
+    after(() => {
+        mongoose.connection.close();
+    });
+
+    context('POST /api/users/', () => {
+
+        it('responds 201 when a user is specified fully', (done) => {
+            request(app)
+                .post('/api/users')
+                .send({
+                    "name": "John Doe",
+                    "avatar": "http://foo.bar/image.png"
+                })
+                .expect(201, done);
+        });
+
+    });
+
+});
