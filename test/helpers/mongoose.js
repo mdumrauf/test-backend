8844f9e3968e691cce4e321545ca815702a8273b
Matias Dumrauf <matias.dumrauf@gmail.com>
2019-02-10 22:41:51 -0300

    removed mongoose helper logging for test

--- test/helpers/mongoose.js
+++ test/helpers/mongoose.js
@@ -11,12 +11,6 @@ class Mongoose {
             mongoose.Promise = Promise;
             mongoose.set('useCreateIndex', true);
             mongoose.connect(mongoDbUri, {useNewUrlParser: true});
-            mongoose.connection.once('open',
-                () => logger.info(
-                    `Mongoose connected to ${mongoose.connection.host}:${mongoose.connection.port}/${mongoose.connection.db.databaseName}`
-                )
-            );
-            mongoose.connection.on('close', () => logger.info('connection closed'));
             mongoose.connection.on('error', err => logger.error(`connection error ${err}`));
 
             return mongoose;
