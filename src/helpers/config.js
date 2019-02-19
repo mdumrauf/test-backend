dcce78e5ba8b60afd3b32dbccf806238c9c633ba
Matias Dumrauf <matias.dumrauf@gmail.com>
2019-02-19 01:12:44 -0300

    parameterized morgan logging level with NODE_ENV

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -4,7 +4,9 @@ const env = process.env.NODE_ENV;
 class Config {
     static configure(app) {
         app.use(require('body-parser').json());
-        app.use(require('morgan')(env && env === 'prod' && 'combined' || 'dev'));
+        if (env !== 'test') {
+            app.use(require('morgan')(env && env === 'prod' && 'combined' || 'dev'));
+        }
     }
 }
 
